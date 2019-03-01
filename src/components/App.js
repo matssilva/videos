a002ac0f44664593e99190e9de5a3657aa01af5b
matss <matss@br.ibm.com>
2019-03-01 16:44:12 -0300

    Styling video list

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
    constructor(props) {
       super(props);
-      this.state = { videos: [] };
+      this.state = { videos: [], selectedVideo: null };
    }
 
    onSearch = async term => {
@@ -15,21 +16,33 @@ class App extends React.Component {
             q: term
          }
       });
-      this.setState({ videos: response.data.items });
+      this.setState({
+         videos: response.data.items,
+         selectedVideo: response.data.items[0]
+      });
    };
 
    onVideoSelect = video => {
-      console.log('From App!', video);
+      this.setState({ selectedVideo: video });
    };
 
    render() {
       return (
          <div className="ui container">
             <SearchBar onSearch={this.onSearch} />
-            <VideoList
-               videos={this.state.videos}
-               onVideoSelect={this.onVideoSelect}
-            />
+            <div className="ui grid">
+               <div className="ui row">
+                  <div className="eleven wide column">
+                     <VideoDetail video={this.state.selectedVideo} />
+                  </div>
+                  <div className="five wide column">
+                     <VideoList
+                        videos={this.state.videos}
+                        onVideoSelect={this.onVideoSelect}
+                     />
+                  </div>
+               </div>
+            </div>
          </div>
       );
    }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,11 @@ import React from 'react';
 const VideoItem = ({ video, onVideoSelect }) => {
    return (
       <div className="video-item item" onClick={() => onVideoSelect(video)}>
-         <img className="ui image" src={video.snippet.thumbnails.default.url} />
+         <img
+            className="ui image"
+            alt={video.snippet.description}
+            src={video.snippet.thumbnails.default.url}
+         />
          <div className="content">
             <div className="header">{video.snippet.title}</div>
          </div>
